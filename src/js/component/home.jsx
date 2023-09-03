a34312fe56fec18f16ced5854d07507956fd1672
LuisMarin-dev <luis.marinprog@gmail.com>
2023-09-03 19:15:58 -0400

    fixed functions for individual and general task deletion. Project complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
   const [text, setText] = useState({"label":"", "done": false});
   
 
-  useEffect(async() => {
-    await createUser();
+  useEffect(() => { // async comented
+    // await createUser();
     getData();
   }
   , [])
@@ -24,8 +24,10 @@ const Home = () => {
       });
       const data = await response.json();
       console.log(data)
-      if(response.ok){
-        setTasks(data);
+      if(response.status == 404){
+        createUser();
+      } else {
+        setTasks(data)
       }
     } catch (error) {
       console.log(error)
@@ -33,6 +35,7 @@ const Home = () => {
   }
 
   const createUser = async() => { //adedd async 9/3/2023
+    try {
     const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/luism', {
       "method": 'POST',
       "headers": {
@@ -41,19 +44,31 @@ const Home = () => {
       "body": JSON.stringify([]) // added 9/3/2023
     });
     console.log(response);
+      if (response.ok){
+        getData()
+      } else {
+        console.log("No se creo el usuario")
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   const postData = async(body) => {
-    const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/luism', {
-      "method": 'PUT',
-      "headers": {
-        "Content-Type": "application/json"
-      },
-      "body": JSON.stringify(body) 
-    })
-    const data = await response.json();
-    console.log(data);
-    return response.ok;
+    try {
+      const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/luism', {
+        "method": 'PUT',
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "body": JSON.stringify(body) 
+      })
+      const data = await response.json();
+      console.log(data);
+      return response.ok;
+    } catch (error) {
+      console,log(error)
+    }
   };
   
   const handleKey = async(event) => {
@@ -69,32 +84,57 @@ const Home = () => {
   };
   
   // al ser presionado esta funcion debe 1: Crear en la linea 45 y el nuevo task creado sera pasado a postdata. Del 46 
-  const deleteTask = async(id) => {
+  // const deleteTask = async(id) => {
+  //   console.log(id)
+  //   if(tasks.lenght > 1) {
+  //     let newTask = tasks.filter((tasks, index) => index !== id);
+  //     console.log(newTask)
+  //     const isPosted = await postData(newTask);
+  //     if(isPosted == true){
+  //       await getData();
+  //       console.log(id)
+  //     }
+  //   } else {
+  //       const isPosted = await deleteUser();
+  //       if(isPosted == true){
+  //         await getData();
+  //       }
+  //       createUser() // added 9/3/2023
+  //     }
+  //   };
     
-    if(tasks.lenght > 1) {
-      let newTask = tasks.filter((tasks, index) => index !== id);
+// ######################## deteleTask de ejemplo
+// const deleteTask = (id) => {
+//   let newTask = tasks.filter((task, index) => index !== id);
+//   setTasks(newTask);
+// };
 
-      const isPosted = await postData(newTask);
-      if(isPosted == true){
-        await getData();
-      }
-    } else {
-        const isPosted = await deleteUser();
-        if(isPosted == true){
-          await getData();
-        }
-        createUser() // added 9/3/2023
-      }
-    };
-    
-    const deleteUser = async() => { //adedd async 9/3/2023
+
+
+
+  const deleteTask = (index) => {
+		const delTask = tasks.filter(text => text.label != tasks[index].label)
+		 setTasks(delTask)
+     postData(delTask);
+	}
+
+  const deleteUser = async() => { //adedd async 9/3/2023
+    try {
       const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/luism', {
         "method": 'DELETE',
         "headers": {
           "Content-Type": "application/json"
         }
-      });
+    });
+      if (response.ok){
+        getData();
+      } else{
+        console.log("no se borro el usuario")
+      }
+    } catch(error) {
+        console.log(error)
     }
+  }
 
 
     return (
@@ -106,7 +146,7 @@ const Home = () => {
           {tasks.map((task, index) => (
             <li key={index}>
               <span>{task.label}</span>
-              <span onClick={() => deleteTask(index)} className="x-button hide">x</span>
+               <span onClick={() => deleteTask(index)} className="x-button hide">x</span> {/* */}
             </li>
           ))}
         </ul>
